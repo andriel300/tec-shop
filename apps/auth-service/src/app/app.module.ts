108db9be9ece81092253ede281f3bc47f9b282c7
andrieljose <andrieldata@proton.me>
2025-09-02 23:35:24 -0300

    SCRUM-53 feat(app): configure main app module
    refactor(app): remove auth controller from main module
    
    This commit configures the main application module to integrate new
    feature modules.
    
    **New Features**
    * **Module Integration**: The `AuthModule`, `RedisModule`, and
    `EmailModule` are now imported into the `AppModule`, making their
    services and controllers available to the application.
    * **Logger Middleware**: The `LoggerMiddleware` is now applied to all
    routes using a more explicit configuration with `RequestMethod.ALL`.
    
    **Refactoring**
    * The `AuthController` was removed from the `controllers` array in
    `AppModule`. This is because it is now part of the `AuthModule`, which
    is imported into `AppModule`. This promotes a more modular and organized
    architecture.

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,26 +1,33 @@
-import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { AuthController } from '../auth/auth.controller';
-import { AllExceptionsFilter, LoggerMiddleware } from '@tec-shop/common'
+import { AuthModule } from '../auth/auth.module';
+import { RedisModule } from '../redis/redis.module';
+import { EmailModule } from '../email/email.module';
+import { AllExceptionsFilter, LoggerMiddleware } from '@tec-shop/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 10,
-    }]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true, // Makes the ConfigService available throughout the app
       envFilePath: './apps/auth-service/.env', // Path to your .env file
     }),
     PrismaModule,
+    AuthModule,
+    RedisModule,
+    EmailModule,
   ],
-  controllers: [AppController, AuthController],
+  controllers: [AppController],
   providers: [
     AppService,
     {
@@ -30,14 +37,11 @@ import { PrismaModule } from '../prisma/prisma.module';
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
-    }
+    },
   ],
 })
-
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL });
   }
 }
