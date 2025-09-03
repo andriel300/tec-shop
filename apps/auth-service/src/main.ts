4a7d0921f9aa4f19ec97dc9950dbfd91354a8b4b
andrieljose <andrieldata@proton.me>
2025-09-02 23:24:45 -0300

    feat(api): add swagger documentation for auth service (SCRUM-53)
    refactor(api): remove verbose comments
    
    This commit adds **Swagger documentation** to the authentication
    service. It configures a global `ValidationPipe` for request validation
    and sets up Swagger at `http://localhost:port/api/docs`.
    
    The previous verbose comments were removed to improve code readability,
    as the new setup is self-explanatory.

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,35 +1,37 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  // Initializes the core application using the root module, which defines
-  // the controllers, providers, and overall structure of this service.
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
 
-  // Configures Cross-Origin Resource Sharing (CORS) to explicitly allow
-  // requests only from the frontend application running locally. This is
-  // essential for secure communication between the frontend and this service
-  // during development, especially when dealing with authentication cookies.
   app.enableCors({
-    origin: ["http://localhost:3000"], // Your frontend dev auth server
+    origin: ["http://localhost:3000"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   });
 
-  // Prepends '/api' to all route paths, effectively namespacing all endpoints.
-  // This helps differentiate API routes from other server-side routes and is
-  // a common practice for API versioning and organization.
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Tec-Shop Auth Service')
+    .setDescription('API for authentication and user management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 6001;
 
-  // Starts the application and begins listening for incoming requests on the specified port.
   await app.listen(port);
 
   Logger.log(`🚀 Auth service is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`📚 Swagger docs available at: http://localhost:${port}/api/docs`);
 }
 
-// Kicks off the entire process. This is the entry point that starts the NestJS application.
 bootstrap();
